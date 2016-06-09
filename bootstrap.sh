efc56d137581e695e4a580355fa7908674167954
Eric Loflin <eric.loflin@SFO-MBP-LOFLIN.fios-router.home>
2016-06-09 11:54:57 -0500

    adding more consul support

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,11 @@ export VM_IP="192.168.205.12"
 export VM_SLAVE_IP="192.168.205.10"
 export VM_SERVER_ID="3"
 vagrant up slave-1 --provision;
+export VM_SERVER="consul-server"
+export VM_IP="192.168.205.20"
+export VM_SLAVE_IP="192.168.205.10"
+export VM_SERVER_ID="4"
+vagrant up consul-server --provision;
 
 echo "Creating Replication on master-1."
 vagrant ssh master-1 -c 'sudo mysql -e "CHANGE MASTER TO MASTER_HOST = '\''192.168.205.11'\'', MASTER_PORT = 3306, MASTER_USER = '\''replication'\'', MASTER_PASSWORD = '\''passw0rd'\'', MASTER_AUTO_POSITION = 1;"';
--- modules/packages/manifests/percona_server.pp
+++ modules/packages/manifests/percona_server.pp
@@ -2,35 +2,53 @@ class packages::percona_server {
 
   if 'master' in $vmserver {
     $mysqlconfig = {
-      'mysqld' => {
-        'bind-address' => '0.0.0.0',
-        'server-id' => $serverid,
-        'log-slave-updates' => '1',
-        'log-bin' => 'mysql-bin',
-        'binlog_format' => 'MIXED',
+      'mysqld'                     => {
+        'bind-address'             => '0.0.0.0',
+        'server-id'                => $serverid,
+        'log-slave-updates'        => '1',
+        'log-bin'                  => 'mysql-bin',
+        'binlog_format'            => 'MIXED',
         'enforce-gtid-consistency' => '',
-        'gtid-mode'=> 'ON',
+        'gtid-mode'                => 'ON',
+        'early-plugin-load'        => 'keyring_file.so',
+        'keyring_file_data'        => '/usr/local/mysql/mysql-keyring/keyring',
       }
     } 
   }
   else {
     $mysqlconfig = {
-      'mysqld' => {
-        'bind-address' => '0.0.0.0',
-        'server-id' => $serverid,
-        'log-bin' => 'mysql-bin',
-        'binlog_format' => 'MIXED',
+      'mysqld'                     => {
+        'bind-address'             => '0.0.0.0',
+        'server-id'                => $serverid,
+        'log-bin'                  => 'mysql-bin',
+        'binlog_format'            => 'MIXED',
         'enforce-gtid-consistency' => '',
-        'gtid-mode'=> 'ON',
-        'relay-log' => 'relay-log-slave',
-        'read-only' => 'ON',
+        'gtid-mode'                => 'ON',
+        'relay-log'                => 'relay-log-slave',
+        'read-only'                => 'ON',
+        'early-plugin-load'        => 'keyring_file.so',
+        'keyring_file_data'        => '/usr/local/mysql/mysql-keyring/keyring',
       }
     } 
   }
 
+  #Used for en encryption and key_ring file tablespace encryption
+  file {'/usr/local/mysql':
+    ensure => 'directory',
+  }
+
+  file {'/usr/local/mysql/mysql-keyring':
+    ensure => 'directory',
+    owner  => 'mysql',
+    group  => 'mysql',
+    mode   => '750',
+  }
+  
+
+
   class { 'mysql::server':
-    root_password   => 'password',
-    package_name    => 'percona-server-server-5.7',
+    root_password    => 'password',
+    package_name     => 'percona-server-server-5.7',
     override_options => $mysqlconfig,
   }
 
@@ -41,7 +59,7 @@ class packages::percona_server {
   }
   #grant replication user
   exec { 'grant-replication-user':
-    command      => '/usr/bin/mysql -e "GRANT REPLICATION SLAVE ON *.* TO \'replication\'@\'%\';"',
+    command      => '/usr/bin/mysql -e "GRANT ALL PRIVILEGES ON *.* TO \'replication\'@\'%\';"',
     notify       =>  Exec['restart-mysql'],
   }
 
