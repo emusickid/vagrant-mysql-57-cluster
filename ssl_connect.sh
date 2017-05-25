0632fe26f6fde1e519616d367a5bf62ae3538f87
Eric Loflin <eric.loflin@SFO-MBP-LOFLIN.local>
2017-05-24 17:07:56 -0700

    foo

--- ssl_connect.sh
+++ ssl_connect.sh
@@ -24,3 +24,20 @@ for SSL in "ssl-mode=REQUIRED" "ssl-mode=DISABLED" ;
 # 	INNER JOIN performance_schema.status_by_thread AS t2 ON t2.thread_id = t.thread_id        
 # WHERE sbt.variable_name = 'Ssl_version' and t2.variable_name = 'Ssl_cipher' 
 # ORDER BY tls_version;
+
+
+#ssh tunnel version
+
+#!/bin/bash
+#mysql -ubenchmark -p -P 3307 -h127.0.0.1 
+ITERATIONS=$1
+for HOST in "192.168.205.10" "127.0.0.1 -P 3307" ;
+	do
+	  STARTTIME=$(date +%s%N)
+	  for ((i=0;i<$ITERATIONS; i++));
+	  do
+	    mysql -h$HOST -ubenchmark -ppassword  --ssl-mode=DISABLED -e "SELECT '$HOST'" > /dev/null 2>&1
+	  done
+	  ENDTIME=$(date +%s%N)
+	  echo "It takes $((($ENDTIME - $STARTTIME)/1000000)) milliseconds to complete \"$ITERATIONS\" \"$HOST\" connections."
+	done
